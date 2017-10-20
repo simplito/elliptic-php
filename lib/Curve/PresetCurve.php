d6c5ecaaf434a52449f5e8faa350311c9fd1dd7a
Łukasz Dudziński <lukdudszinski@simplito.com>
2017-10-20 12:29:19 +0200

    Init commit

---
+++ lib/Curve/PresetCurve.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Elliptic\Curve;
+
+use Elliptic\Utils;
+use BN\BN;
+
+class PresetCurve
+{
+    public $curve;
+    public $g;
+    public $n;
+    public $hash;
+
+    function __construct($options)
+    {
+        if ( $options["type"] === "short" )
+            $this->curve = new ShortCurve($options);
+        elseif ( $options["type"] === "edwards" )
+            $this->curve = new EdwardsCurve($options);
+        else
+            $this->curve = new MontCurve($options);
+
+        $this->g = $this->curve->g;
+        $this->n = $this->curve->n;
+        $this->hash = isset($options["hash"]) ? $options["hash"] : null;
+
+        //assert('$this->g->validate()'); //, "Invalid curve");
+        //assert('$this->g->mul($this->n)->isInfinity()'); //, "Invalid curve, G*N != O");
+    }
+}
+
+?>
